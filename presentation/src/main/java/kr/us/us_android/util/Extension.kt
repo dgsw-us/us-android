845bf07ea7dfd98fec800471dea59a419e7a293a
seo1016 <seoseunghun3@gmail.com>
2024-07-16 16:57:53 +0900

    Feat: Extension 추가(toast)

---
+++ presentation/src/main/java/kr/us/us_android/util/Extension.kt
@@ -0,0 +1,10 @@
+package kr.us.us_android.util
+
+import android.content.Context
+import android.widget.Toast
+
+internal fun Context.shortToast(text: String) =
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+internal fun Context.longToast(text: String) =
+    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
