fac767bebf7d538a1de617b0fbcd97208c2144d4
seo1016 <seoseunghun3@gmail.com>
2024-07-16 16:06:34 +0900

    Refactor: 불필요한 import 삭제

--- presentation/src/main/java/kr/us/us_android/MainActivity.kt
+++ presentation/src/main/java/kr/us/us_android/MainActivity.kt
@@ -1,10 +1,7 @@
 package kr.us.us_android
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kr.us.us_android.databinding.ActivityMainBinding
 
