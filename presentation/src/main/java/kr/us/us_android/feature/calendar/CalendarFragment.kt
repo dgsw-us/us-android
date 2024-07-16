5dad305c020acb31232272f78d465b5807e77bbf
seo1016 <seoseunghun3@gmail.com>
2024-07-16 19:41:47 +0900

    Feat: fragment_calendar view

--- presentation/src/main/java/kr/us/us_android/feature/calendar/CalendarFragment.kt
+++ presentation/src/main/java/kr/us/us_android/feature/calendar/CalendarFragment.kt
@@ -5,18 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import kr.us.us_android.R
 import kr.us.us_android.databinding.FragmentCalendarBinding
 
-class CalendarFragment : Fragment() {
+class CalendarFragment : Fragment(R.layout.fragment_calendar) {
 
-    private lateinit var binding: FragmentCalendarBinding
+    private var _binding: FragmentCalendarBinding ?= null
+    private lateinit var recyclerView: RecyclerView
 
+    private val binding get() = _binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+    ): View? {
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
 
-        return binding.root
+        val view = binding?.root
+
+        initView(_binding!!)
+        createData()
+
+        return view
+    }
+
+    private fun initView(binding: FragmentCalendarBinding) {
+        recyclerView = binding.calRecycler
+        var position: Int = Int.MAX_VALUE / 2
+
+        binding.calRecycler.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.calRecycler.adapter = MonthAdapter()
+
+        //item의 위치 지정
+        binding.calRecycler.scrollToPosition(position)
+
+        //항목씩 스크롤 지정
+        val snap = PagerSnapHelper()
+        snap.attachToRecyclerView(binding.calRecycler)
+    }
+
+    private fun createData() {
+        binding?.calRecycler?.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
